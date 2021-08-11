fd67c6176739f17f72bcf4bc74d4c7f90e0fbc76
Miroslav Popov <info@forexsb.com>
2021-08-11 07:29:44 +0000

    KIM-1 ROM finalised.

--- kim/export-kim-1-rom.js
+++ kim/export-kim-1-rom.js
@@ -5,25 +5,16 @@ const fs = require('fs')
 const { Assembler } = require('../js')
 const { Utils } = require('../js')
 
-const sourceDir  =  (__dirname).endsWith('kim') ? __dirname : __dirname + '/kim'
-const sourcePath = sourceDir + '/kim-1-6530-002.asm'
-const sourceCode = fs.readFileSync(sourcePath, 'utf8')
+const sourceDir   =  (__dirname).endsWith('kim') ? __dirname : __dirname + '/kim'
+const sourcePath  = sourceDir + '/kim-1-6530-002.asm'
+const sourceCode  = fs.readFileSync(sourcePath, 'utf8')
+const binFilePath = sourceDir + '/bin/rom-32k.bin'
 
 const assembler = new Assembler()
 const codePages = assembler.assemble(sourceCode)
 
-const romSize  =  2 * 1024
 const buffSize = 32 * 1024
-const buffer   = Buffer.alloc(buffSize)
-
-for (let offset = 0; offset < romSize; offset++) {
-	buffer.writeUInt8(0x00, offset)
-}
-
-for (let offset = romSize; offset < romSize; offset++) {
-	buffer.writeUInt8(0xFF, offset)
-}
-
+const buffer   = Buffer.alloc(buffSize, 0xFF)
 const romStart = 0x1800   // 0001 1000 0000 0000
 
 for (const pageTag of Object.keys(codePages)) {
@@ -32,21 +23,21 @@ for (const pageTag of Object.keys(codePages)) {
 		const value = codePages[pageTag][offset]
 		if (typeof value === 'number') {
 			const address = pageAddress + offset
-			const valHex = Utils.byteToHex(value)
+			const valHex  = Utils.byteToHex(value)
 			const val = parseInt(valHex, 16)
-			buffer.writeUInt8(val , address - romStart)
+			buffer.writeUInt8(val, address - romStart)
 		}
 	}
 }
 
-const filePath = sourceDir + '/bin/rom-32k.bin'
 
-fs.writeFileSync(filePath, buffer, 'binary')
+fs.writeFileSync(binFilePath, buffer, 'binary')
 
-const codeDto = assembler.tokenize(sourceCode)
+const codeDto    = assembler.tokenize(sourceCode)
 const instTokens = assembler.parseInstructions(codeDto)
 assembler.resolveUnsetLabels(codeDto, instTokens)
 
-Object.keys(codeDto.labels).forEach( key =>  {
+// Export labels
+Object.keys(codeDto.labels).sort().forEach( key =>  {
 	console.log( `${key.toUpperCase().padEnd(8, ' ')} ${codeDto.labels[key].toString(16).toUpperCase()}`)
 })
--- kim/kim-1-6530-002.asm
+++ kim/kim-1-6530-002.asm
@@ -1,33 +1,39 @@
 ;
 ; KIM-1 Source Code
 ;
-        PCL    = $EF   ; program counter low
-        PCH    = $F0   ; program counter high
-        PREG   = $F1   ; status register
-        SPUSER = $F2   ; stack pointer
-        ACC    = $F3
-        YREG   = $F4
-        XREG   = $F5
-        INL    = $F8   ; input buffer low
-        INH    = $F9   ; input buffer high
-        POINTL = $FA   ; address L on display
-        POINTH = $FB   ; address H on display
-        TEMP   = $FC  
-        TMPX   = $FD  
-        MODE   = $FF  
-;                     
-        SBD    = $1740 ; I/O B register
-        SAD    = $1741 ; I/O A register
-        PBDD   = $1742 ; I/O B direction
-        PADD   = $1743 ; I/O A direction
-
+; MPU reg. save area in Page 0
+        PCL     = $EF   ; Program counter low
+        PCH     = $F0   ; Program counter high
+        PREG    = $F1   ; Current status register
+        SPUSER  = $F2   ; Current stack pointer
+        ACC     = $F3   ; Accumulator
+        YREG    = $F4   ; Y index
+        XREG    = $F5   ; X index
+; KIM fixed area in Page 0
+        INL     = $F8   ; Input buffer low
+        INH     = $F9   ; Input buffer high
+        POINTL  = $FA   ; LSB of address on display
+        POINTH  = $FB   ; MSB of address on display
+        TEMP    = $FC
+        TMPX    = $FD
+        MODE    = $FF   ; Keboard mode: 0 = data, 1 = addres,
+;
+        SBD     = $1740 ; I/O B register
+        SAD     = $174F ; I/O A register
+        PBDD    = $1742 ; I/O B direction
+        PADD    = $1743 ; I/O A direction
+; Interrupt vectors
+        NMIV    = $17FA ; STOP VECTOR (STOP=1C00)
+        RSTV    = $17FC ; RST VECTOR
+        IRQV    = $17FE ; IRQ VECTOR (BRK=1C00)
+;
 *=$1C00
 ;
 ; KIM-entry via NMI or IRQ
-SAVE    STA     ACC
+SAVE    STA     ACC     ;                               1C00
         PLA
         STA     PREG
-        PLA
+        PLA             ; KIM entry via JSR (A lost)    1C05
         STA     PCL
         STA     POINTL
         PLA
@@ -40,53 +46,47 @@ SAVE    STA     ACC
         JSR     INITS
         JMP     START
 ;
+NMIT    JMP     (NMIV)  ; Non-maskable interrupt trap   1C1C
+IRQT    JMP     (IRQV)  ; Interrupt trap                1C1F
 ; The KIM starts here after a reset
-RESET   LDX     #$FF
+RST     LDX     #$FF    ;                               1C22
         TXS             ; set stack
         STX     SPUSER
         JSR     INITS
         JMP     START
-;
-; Initialization 6520
-INITS   LDX     #$01
-        STX     MODE    ; Set display to address mode
-INIT1   LDX     #$00
-        STX     PADD    ; PA0..PA7 = input
-        LDX     #$3F
-        STX     PBDD    ; PB0..PB5 = output
-        LDX     #$07    ; Enable data in
-        STX     SBD     ; Output
-        CLD
-        SEI
-        RTS
-;
+*=$1C4F
+; Make TTY / KB selection (currently only KB)
+START   JSR     INIT1   ;                               1C4F
+        JMP     TTYKB
+*=$1C77
 ; Main routine for keyboard and display
-START   JSR     INIT1
-        JSR     SCAND   ; wait until NO key pressed
+TTYKB   JSR     SCAND   ; Wait until NO key pressed     1C77
         BNE     START   ; if pressed, wait again ->
-START1  JSR     SCAND   ; Wait for key...
-        BEQ     START1  ; no key ->
+TTYKB1  LDA     #$01    ; check KB/TTY mode
+        BIT     SAD     ; TTY?
+		BEQ     START   ; yes ->
+        JSR     SCAND   ; Wait for key...
+        BEQ     TTYKB1  ; no key ->
         JSR     SCAND   ; debounce key
-        BEQ     START1  ; no key ->
-        JSR     GETKEY
+        BEQ     TTYKB1  ; no key ->
+GETK    JSR     GETKEY
         CMP     #$15    ; >= $15 = illegal - no kew pressed
         BPL     START   ; yes ->
+        CMP     #$14
+        BEQ     PCCMD   ; "PC" - display Program Counter
         CMP     #$10
         BEQ     ADDRM   ; "AD" - addres mode
         CMP     #$11
         BEQ     DATAM   ; "DA" - data mode
-        CMP     #$14
-        BEQ     PCCMD   ; "PC" - display Program Counter
         CMP     #$12
         BEQ     STEP    ; "+" - step
         CMP     #$13
         BEQ     GOV     ; "GO" - execute program
-; One of the hexidecimal buttons has been pushed
+DATA    ASL             ; One of the hexadecimal buttons has been pushed
         ASL             ; move LSB key number to MSB
         ASL
         ASL
-        ASL
-        STA     TEMP    ; store for datamode
+        STA     TEMP    ; store for data mode
         LDX     #$04
 DATA1   LDY     MODE    ; part of address?
         BNE     ADDR    ; yes ->
@@ -101,17 +101,23 @@ ADDR    ASL             ; TEMP not needed here
         ROL     POINTH  ; POINTL -> POINTH
 DATA2   DEX             ; 4 times = complete nibble?
         BNE     DATA1   ; no ->
-        JMP     START
+        BEQ     DATAM2  ; -> always
 ;
 ; "AD" - Switch to address mode
 ADDRM   LDA     #$01
-        STA     MODE
-        JMP     START
+        BNE     DATAM1  ; -> always
 ;
 ; "DA" -  Switch to data mode
 DATAM   LDA     #$00
-        STA     MODE
-        JMP     START
+DATAM1  STA     MODE
+DATAM2  JMP     START
+;
+; "+" key. Increment address on display
+STEP    JSR     INCPT
+		JMP     START
+;
+; "GO" - Start a program at displayed address.
+GOV     JMP     GOEXEC
 ;
 ; "PC" - Display PC by moving it to POINT
 PCCMD   LDA     PCL
@@ -119,17 +125,10 @@ PCCMD   LDA     PCL
         LDA     PCH
         STA     POINTH
         JMP     START
+*=$1DC8
 ;
-; "+" key. Increment address on display
-; It is a 2-byte word so increment Low first. Increment High if necessery.
-STEP    INC     POINTL ; Increment the Low byte.
-        BNE     STEP1  ; Check if 0. If not, branch to end.
-        INC     POINTH ; If Low byte equal to 0, increment the High byte.
-STEP1   JMP     START
-;
-; "GO" - Start a program at displayed address.
 ; RTI is used as a comfortable way to define all flags in one move.
-GOV     LDX     SPUSER  ; user user defined stack
+GOEXEC  LDX     SPUSER  ; user user defined stack
         TXS
         LDA     POINTH  ; program runs from
         PHA             ;  displayed address
@@ -141,9 +140,22 @@ GOV     LDX     SPUSER  ; user user defined stack
         LDY     YREG
         LDA     ACC
         RTI             ; start program
-;
+*=$1E88
+; Initialization 6520
+INITS   LDX     #$01    ;                                        1E88
+        STX     MODE    ; Set display to address mode
+INIT1   LDX     #$00    ; 0000 0000
+        STX     PADD    ; PA0..PA7 = input
+        LDX     #$3F    ; 0011 1111
+        STX     PBDD    ; PB0..PB5 = output. (currently used PB1..PB4)
+        LDX     #$07    ; 0000 0111
+        STX     SBD     ; U24 (74LS145) outputs 3 to check the KBD/TTY mode
+        CLD
+        SEI
+        RTS
+*=$1EFE
 ; Determine if key is depressed: NO -> A=0, YES -> A>0
-AK      LDY     #$03    ; 3 rows
+AK      LDY     #$03    ; 3 rows                                1EFE
         LDX     #$01    ; select 74145 output 0
 ONEKEY  LDA     #$FF    ; initial value
 AK1     STX     SBD     ; enable output = select row
@@ -159,10 +171,10 @@ AK1     STX     SBD     ; enable output = select row
         RTS
 ;
 ; Output to 7-segment-display
-SCAND   LDY     #$00    ; POINTL/POINTH = address on display
+SCAND   LDY     #$00    ; POINTL/POINTH = address on display    1F19
         LDA     (POINTL),Y ; get data from this address
         STA     INH     ; store in INH =
-        LDA     #$7F    ; PA0..PA6 := output
+SCANDS  LDA     #$7F    ; PA0..PA6 := output
         STA     PADD
         LDX     #$09    ; Start with display at output 4
         LDY     #$03    ; 3 bytes to be shown
@@ -178,10 +190,9 @@ SCAND1  LDA     INL,Y   ; get byte
         DEY             ; all ?
         BNE     SCAND1  ; no ->
         STX     SBD     ; all digits off
-        LDA     #$00    ; Change segment 
+        LDA     #$00    ; Change segment
         STA     PADD    ; PA0..PA7 := input
         JMP     AK      ; GET ANY KEY
-;
 ; Convert digit into 7-segment-value
 CONVD   STY     TEMP
         TAY
@@ -190,16 +201,21 @@ CONVD   STY     TEMP
         STY     SAD     ; turn off segments
         STX     SBD     ; select 7-s-display
         STA     SAD     ; output code on display
-        LDY     #$49    ; #$7F delay ~500 cycles
+        LDY     #$3F    ; #$7F delay ~500 cycles
 CONVD1  DEY
         BNE     CONVD1
         INX             ; Get next digit number
         INX             ; Add 2
         LDY     TEMP    ; Restore Y
         RTS
-;
+; Increment POINT = address on display
+INCPT   INC     POINTL
+		BNE     INCPT2
+
+		INC     POINTH
+INCPT2  RTS
 ; Get key from keyboard in A
-GETKEY  LDX     #$21    ; row 0
+GETKEY  LDX     #$21    ; row 0                                1F6A
 GETKE5  LDY     #$01    ; only one row in the time
         JSR     ONEKEY  ; key?
         BNE     KEYIN   ; yes ->
@@ -210,9 +226,6 @@ GETKE5  LDY     #$01    ; only one row in the time
 KEYIN   LDY     #$FF    ; Y := key number
 KEYIN1  ASL             ; shift A until
         BCS     KEYIN2  ;  bit = 1 ->
-;
-; Comment: bit 7 is always 0 so Carry is always 0 the first time
-;  and allowing Y to become 0 (key $FF does not exist)
         INY
         BPL     KEYIN1  ; always ->
 KEYIN2  TXA
@@ -221,9 +234,7 @@ KEYIN2  TXA
         TAX             ; X := actual row+1
         TYA
         BPL     KEYIN4  ; always, because Y<7 ->
-;
-; Add 7 to A for every row above 0 to get actual key number
-KEYIN3  CLC
+KEYIN3  CLC             ; Add 7 to A for every row above 0 to get actual key number
         ADC     #$07    ; add (X-1) times 7 to A
 KEYIN4  DEX             ; countdown to 0
         BNE     KEYIN3
@@ -238,6 +249,6 @@ TABLE   .BYTE   $BF, $86, $DB, $CF, $E6, $ED, $FD, $87
 ;
 ; Entry vectors
 *=$1FFA
-        .WORD   SAVE    ; NMI
-        .WORD   RESET   ; Reset
-        .WORD   SAVE    ; IRQ
+        .WORD   NMIT    ; NMI
+        .WORD   RST     ; Reset
+        .WORD   IRQT    ; IRQ
